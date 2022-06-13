0d50af5440bba15276effc93b72c08e0ca37afa5
migmedia <coding@migmedia.de>
2022-06-13 17:35:48 +0200

    refactored and edited to use actual crates.

--- src/main.rs
+++ src/main.rs
@@ -2,53 +2,49 @@
 ///! A zfs-auto-snapshot like tool written in Rust.
 ///!
 ///! License: MIT
-///! (c) migmedia 2020
-extern crate chrono;
-extern crate derive_builder;
-extern crate log;
-extern crate simplelog;
+///! (c) migmedia 2020 - 2022
 mod zfs;
 
-use crate::zfs::FsType;
+use crate::zfs::{FsType, Zfs};
 use chrono::{DateTime, Utc};
+use clap::Parser;
 use log::{debug, error};
-use simplelog::{CombinedLogger, Config, LevelFilter, TermLogger, TerminalMode};
-use structopt::StructOpt;
+use simplelog::{ColorChoice, CombinedLogger, Config, LevelFilter, TermLogger, TerminalMode};
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "zfs-autosnaprs", about = "A zfs snapshot managing utility.")]
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
 struct Opt {
     /// Prints info messages.
-    #[structopt(short, long)]
+    #[clap(short, long)]
     pub verbose: bool,
 
     /// Prints debug messages.
-    #[structopt(short, long)]
+    #[clap(short, long)]
     pub debug: bool,
 
     /// Label of snapshot usually 'hourly', 'daily', or 'monthly'.
-    #[structopt(name = "LAB", short, long = "label")]
+    #[clap(name = "LAB", short, long = "label")]
     pub label: String,
 
     /// Min size in Kilo-Byte.
-    #[structopt(short = "m", long)]
+    #[clap(short = 'm', long)]
     pub min_size: Option<usize>,
 
     /// Keeps NUM recent snapshots and destroy older snapshots.
-    #[structopt(name = "NUM", short, long = "keep", default_value = "8")]
+    #[clap(name = "NUM", short, long = "keep", default_value = "8")]
     pub keep: usize,
 
     /// Prefix of snapshots.
-    #[structopt(short, long, default_value = "zfs-snapshot")]
+    #[clap(short, long, default_value = "zfs-snapshot")]
     pub prefix: String,
 
     /// Prints actions without actually doing anything.
-    #[structopt(short = "n", long = "dry-run")]
+    #[clap(short = 'n', long = "dry-run")]
     pub dry_run: bool,
 }
 
 fn main() {
-    let opt = Opt::from_args();
+    let opt = Opt::parse();
     CombinedLogger::init(vec![TermLogger::new(
         match opt.debug {
             true => LevelFilter::Debug,
@@ -59,38 +55,40 @@ fn main() {
         },
         Config::default(),
         TerminalMode::Mixed,
-    )
-    .unwrap()])
+        ColorChoice::Auto,
+    )])
     .unwrap();
     let now: DateTime<Utc> = Utc::now();
-    let zfs = zfs::ZfsBuilder::default()
-        .prefix(opt.prefix)
-        .label(opt.label)
-        .history(opt.keep)
-        .date(format!("{}", now.format("%Y-%m-%d-%H%M")))
-        .build()
-        .unwrap();
-    let snsl = zfs.list_filesystems(FsType::Snapshot);
+    let zfs = Zfs::new(
+        opt.dry_run,
+        opt.prefix,
+        opt.label,
+        opt.keep,
+        now.format("%Y-%m-%d-%H%M").to_string(),
+    );
+    let snapshots = zfs.list_filesystems(FsType::Snapshot);
     for fs in zfs
         .list_filesystems(FsType::Filesystem)
         .iter()
         .filter(|f| f.snap)
     {
-        debug!("FS: {}", fs.name);
-        let (exp_fs, size) = zfs.find_expendable_snapshots(&fs, &snsl);
-        if opt.min_size == None || size > opt.min_size.unwrap() {
-            match zfs.create_snapshot(fs) {
-                Ok(()) => {
-                    debug!("Created! {:?}", &exp_fs);
-                    for exp in exp_fs {
-                        match zfs.remove_snapshot(exp) {
-                            Ok(()) => {}
-                            Err(e) => error!("{}", e),
-                        };
-                    }
+        if opt.min_size.is_some() && !zfs.snapshot_needed(opt.min_size.unwrap(), fs, &snapshots) {
+            debug!("skip FS: {:?}", fs);
+            continue;
+        }
+        let exp_fs = zfs.find_expendable_snapshots(fs, &snapshots);
+        debug!("FS: {:?}", fs);
+        match zfs.create_snapshot(fs) {
+            Ok(()) => {
+                debug!("Created! {:?}", &exp_fs);
+                for exp in exp_fs {
+                    match zfs.remove_snapshot(exp) {
+                        Ok(()) => {}
+                        Err(e) => error!("{}", e),
+                    };
                 }
-                Err(e) => error!("{}", e),
             }
+            Err(e) => error!("{}", e),
         }
     }
 }
--- src/zfs.rs
+++ src/zfs.rs
@@ -1,6 +1,6 @@
 ///! zfs
 ///! Helper-objects for dealing with zfs.  
-use derive_builder::*;
+use chrono::{DateTime, NaiveDateTime, Utc};
 use log::{debug, info};
 use std::env;
 use std::ffi::OsStr;
@@ -10,6 +10,7 @@ use std::process;
 #[derive(Eq, PartialEq, Debug)]
 pub struct FS {
     pub name: String,
+    date: DateTime<Utc>,
     fs_type: FsType,
     pub snap: bool,
     written: usize,
@@ -29,7 +30,7 @@ pub enum FsType {
 }
 
 impl FsType {
-    fn to_str(&self) -> &str {
+    fn to_str(&self) -> &'static str {
         match self {
             FsType::Snapshot => "snapshot",
             FsType::Filesystem => "filesystem",
@@ -46,16 +47,10 @@ impl fmt::Display for FsType {
 /// Object for working with the zfs-binary. Offers methods to analyse zfs.
 ///
 ///
-#[derive(Builder)]
-#[builder(setter(into))]
 pub struct Zfs {
-    #[builder(default = "self.default_exec()")]
     executable: String,
-    #[builder(default = "false")]
     pretend: bool,
-    #[builder(default = "self.default_prefix()")]
     prefix: String,
-    #[builder(default = "self.default_option_name()")]
     option_name: String,
     label: String,
     history: usize,
@@ -63,6 +58,23 @@ pub struct Zfs {
 }
 
 impl Zfs {
+    pub fn new<P, L, D>(pretend: bool, prefix: P, label: L, history: usize, date: D) -> Self
+    where
+        P: Into<String>,
+        L: Into<String>,
+        D: Into<String>,
+    {
+        Self {
+            executable: default_exec(),
+            pretend,
+            prefix: prefix.into(),
+            option_name: String::from("com.sun:auto-snapshot"),
+            label: label.into(),
+            history,
+            date: date.into(),
+        }
+    }
+
     fn cmd(&self) -> process::Command {
         process::Command::new(&self.executable)
     }
@@ -74,29 +86,26 @@ impl Zfs {
     /// * fs - filesystem to snap over
     /// * snaps - List of snapshots, to filter and return.
     ///
-    pub fn find_expendable_snapshots<'a>(
-        &'a self,
-        fs: &FS,
-        snaps: &'a Vec<FS>,
-    ) -> (Vec<&FS>, usize) {
-        let name = format!("{}@{}-{}", fs.name, self.prefix, self.label);
+    pub fn find_expendable_snapshots<'a>(&'a self, fs: &FS, snaps: &'a [FS]) -> Vec<&'a FS> {
+        let snaps = Self::filter_snaps(fs, snaps);
+        // skip amount to hold, return the rest.
+        snaps.iter().skip(self.history).copied().collect()
+    }
 
+    fn filter_snaps<'a>(fs: &FS, snaps: &'a [FS]) -> Vec<&'a FS> {
         // filter snaps-list fitting to fs.
-        let mut snaps: Vec<&FS> = snaps.iter().filter(|&sn| sn.fs == name).collect();
+        let mut snaps: Vec<&FS> = snaps.iter().filter(|&sn| sn.fs == fs.name).collect();
+        // Sort descending by FS.date
+        snaps.sort_unstable_by(|&a, &b| b.date.cmp(&a.date));
+        snaps
+    }
 
-        // Sort descending by FS.name
-        snaps.sort_unstable_by(|&a, &b| b.name.cmp(&a.name));
-
-        let size = match snaps.iter().next() {
-            None => usize::max_value(),
-            Some(&fs) => fs.written,
-        };
-
-        (
-            // skip amount to hold, return the rest.
-            snaps.iter().skip(self.history).map(|t| t.clone()).collect(),
-            size,
-        )
+    pub fn snapshot_needed<'a>(&'a self, min_size: usize, fs: &FS, snaps: &'a [FS]) -> bool {
+        let snaps = Self::filter_snaps(fs, snaps);
+        match snaps.last() {
+            Some(&fs) => fs.written > min_size,
+            None => true,
+        }
     }
 
     /// Returns a list of the filesystems provided by the local zfs.
@@ -111,7 +120,7 @@ impl Zfs {
             "-Hp",
             "-o",
             &format!(
-                "name,used,{op},{op}:{suf}",
+                "name,used,{op},{op}:{suf},creation",
                 op = &self.option_name,
                 suf = &self.label
             ),
@@ -127,7 +136,7 @@ impl Zfs {
         let stdout = String::from_utf8(ret.stdout).unwrap();
         let lines: Vec<FS> = stdout
             .split('\n')
-            .filter(|l| l.len() > 0)
+            .filter(|l| !l.is_empty())
             .map(|t| str2fs(t, fst))
             .collect();
         lines
@@ -140,10 +149,14 @@ impl Zfs {
     /// * fs - filesystem to snap over
     ///
     pub fn create_snapshot(&self, fs: &FS) -> Result<(), std::io::Error> {
-        let args = &[
-            "snapshot",
-            &format!("{}@{}-{}_{}", fs.name, self.prefix, self.label, self.date),
-        ];
+        let FS { name, .. } = fs;
+        let Self {
+            prefix,
+            label,
+            date,
+            ..
+        } = self;
+        let args = &["snapshot", &format!("{name}@{prefix}-{label}_{date}")];
         info!("zfs {}", args.join(" "));
         if !self.pretend {
             let output = self.cmd().args(args).output()?;
@@ -158,7 +171,12 @@ impl Zfs {
     ///
     /// * fs - filesystem to destroy
     ///
-    pub fn remove_snapshot(&self, fs: &FS) -> Result<(), std::io::Error> {
+    pub fn remove_snapshot(&self, fs: &FS) -> Result<(), ZfsError> {
+        if !fs.snap {
+            return Err(ZfsError::InternalError(
+                "Filesystems can't be removed!".into(),
+            ));
+        }
         let args = &["destroy", &fs.name];
         info!("zfs {}", args.join(" "));
         if !self.pretend {
@@ -169,87 +187,101 @@ impl Zfs {
     }
 }
 
-impl ZfsBuilder {
-    fn default_exec(&self) -> String {
-        env::var_os("ZFS_CMD")
-            .unwrap_or(OsStr::new("zfs").to_owned())
-            .to_str()
-            .unwrap()
-            .to_owned()
-    }
-
-    fn default_option_name(&self) -> String {
-        String::from("com.sun:auto-snapshot")
-    }
-
-    fn default_prefix(&self) -> String {
-        String::from("zfs-snapshot")
-    }
+pub fn default_exec() -> String {
+    env::var_os("ZFS_CMD")
+        .unwrap_or_else(|| OsStr::new("zfs").to_owned())
+        .to_str()
+        .unwrap()
+        .to_owned()
 }
 
 fn eval(opt: Option<&&str>) -> bool {
     match opt {
-        Some(&opt) => match opt {
-            "true" => true,
-            _ => false,
-        },
+        Some(&opt) => opt == "true",
         None => false,
     }
 }
 
-fn str2fs(str: &str, fst: FsType) -> FS {
-    let p: Vec<&str> = str.split("\t").collect();
+fn str2fs(str: &str, fs_type: FsType) -> FS {
+    let p: Vec<&str> = str.split('\t').collect();
     let name = p[0].to_string();
+    let date = DateTime::from_utc(
+        NaiveDateTime::from_timestamp(p[4].parse().unwrap_or_default(), 0),
+        Utc,
+    );
     FS {
         name: name.clone(),
-        written: p[1].parse().unwrap(),
+        written: p[1].parse().unwrap_or_default(),
         snap: eval(p.get(2)) || eval(p.get(3)),
-        fs_type: fst,
-        fs: match fst {
-            FsType::Filesystem => String::from(name),
-            FsType::Snapshot => name.split("_").next().unwrap().to_string(),
+        date,
+        fs_type,
+        fs: match fs_type {
+            FsType::Filesystem => name,
+            FsType::Snapshot => name.split('@').next().unwrap().to_string(),
         },
+    }
+}
+
+pub enum ZfsError {
+    IOError(std::io::Error),
+    InternalError(String),
+}
+
+impl From<std::io::Error> for ZfsError {
+    fn from(e: std::io::Error) -> Self {
+        ZfsError::IOError(e)
+    }
+}
+
+impl fmt::Display for ZfsError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ZfsError::IOError(e) => write!(f, "{}", e),
+            ZfsError::InternalError(s) => write!(f, "{}", s),
+        }
     }
 }
 
 #[test]
 fn test_str2fs() {
-    let fs = str2fs("tank\t24576\t-\t-".into(), FsType::Filesystem);
+    let fs = str2fs("tank\t24576\t-\t-\t1608216521", FsType::Filesystem);
     assert_eq!(fs.name, String::from("tank"));
     assert_eq!(fs.written, 24576usize);
-    assert_eq!(fs.snap, false);
-    let fs = str2fs("tank\t24576\t-\ttrue".into(), FsType::Filesystem);
-    assert_eq!(fs.snap, true);
-    let fs = str2fs("tank\t24576\ttrue\tfalse".into(), FsType::Filesystem);
-    assert_eq!(fs.snap, true);
-    let fs = str2fs("tank\t24576\ttrue\ttrue".into(), FsType::Filesystem);
-    assert_eq!(fs.snap, true);
+    assert!(!fs.snap);
+    let fs = str2fs("tank\t24576\t-\ttrue\t1608216521", FsType::Filesystem);
+    assert!(fs.snap);
+    let fs = str2fs("tank\t24576\ttrue\tfalse\t1608216521", FsType::Filesystem);
+    assert!(fs.snap);
+    let fs = str2fs("tank\t24576\ttrue\ttrue\t1608216521", FsType::Filesystem);
+    assert!(fs.snap);
 }
 
 #[test]
 fn test_zfs_find_expendable_snapshots() {
-    let zfs = ZfsBuilder::default()
-        .label("weekly")
-        .history(1usize)
-        .date(String::from("2019-12-30_1807"))
-        .build()
-        .unwrap();
+    let zfs = Zfs::new(true, "zfs-snapshot", "weekly", 1usize, "2019-12-30_1807");
     let fs_snaps = vec![
         str2fs(
-            "tank/SRV/www@zfs-snapshot-weekly_2019-12-30_1207\t23234\t-\t-".into(),
+            "tank/SRV/www@zfs-snapshot-weekly_2019-12-30_1207\t23234\t-\t-\t1608216421",
             FsType::Snapshot,
         ),
         str2fs(
-            "tank/SRV/www@zfs-snapshot-weekly_2019-12-30_1407\t245643\t-\t-".into(),
+            "tank/SRV/www@zfs-snapshot-weekly_2019-12-30_1907\t245643\t-\t-\t1608216921",
             FsType::Snapshot,
         ),
         str2fs(
-            "tank/SRV/www@zfs-snapshot-weekly_2019-12-30_1607\t12340\t-\t-".into(),
+            "tank/SRV/www@zfs-snapshot-weekly_2019-12-30_1607\t12340\t-\t-\t1608216821",
             FsType::Snapshot,
         ),
     ];
-    let fs_orig = str2fs("tank/SRV/www\t245643\t-\t-".into(), FsType::Filesystem);
-    let (fs, size) = zfs.find_expendable_snapshots(&fs_orig, &fs_snaps);
+    let fs_orig = str2fs("tank/SRV/www\t245643\t-\t-\t121212112", FsType::Filesystem);
+    let expendables = zfs.find_expendable_snapshots(&fs_orig, &fs_snaps);
 
-    assert_eq!(size, 12340usize);
+    assert_eq!(
+        expendables.first().unwrap().name,
+        "tank/SRV/www@zfs-snapshot-weekly_2019-12-30_1607"
+    );
+    assert_eq!(
+        expendables.get(1).unwrap().name,
+        "tank/SRV/www@zfs-snapshot-weekly_2019-12-30_1207"
+    );
 }
